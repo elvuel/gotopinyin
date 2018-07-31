48d15b92c79d0f551e2af3a7f4399d94acda051e
elvuel <elvuel@gmail.com>
2018-07-31 11:11:44 +0800

    gofmt

--- gotopinyin_test.go
+++ gotopinyin_test.go
@@ -4,44 +4,44 @@ import (
 	"testing"
 )
 
-var fixturesWithSpace = map[string]string {
-	"": "",
-	" ": " ",
-	"a": "a",
-	" a": " a",
-	"a ": "a ",
-	" a ": " a ",
-	"abc": "abc",
-	"a bc": "a bc",
-	"汉": "han",
-	"汉 ": "han ",
-	" 汉": " han",
-	"汉 字": "han zi",
-	" 汉 字": " han zi",
-	" 汉 字 ": " han zi ",
+var fixturesWithSpace = map[string]string{
+	"":        "",
+	" ":       " ",
+	"a":       "a",
+	" a":      " a",
+	"a ":      "a ",
+	" a ":     " a ",
+	"abc":     "abc",
+	"a bc":    "a bc",
+	"汉":       "han",
+	"汉 ":      "han ",
+	" 汉":      " han",
+	"汉 字":     "han zi",
+	" 汉 字":    " han zi",
+	" 汉 字 ":   " han zi ",
 	"HAN汉ZI字": "HAN han ZI zi",
 }
 
-var fixturesWithDash = map[string]string {
-	"": "",
-	" ": " ",
-	"a": "a",
-	" a": " a",
-	"a ": "a ",
-	" a ": " a ",
-	"abc": "abc",
-	"a bc": "a bc",
-	"汉": "han",
-	"汉 ": "han ",
-	" 汉": " han",
-	"汉 字": "han zi",
-	" 汉 字": " han zi",
-	" 汉 字 ": " han zi ",
+var fixturesWithDash = map[string]string{
+	"":        "",
+	" ":       " ",
+	"a":       "a",
+	" a":      " a",
+	"a ":      "a ",
+	" a ":     " a ",
+	"abc":     "abc",
+	"a bc":    "a bc",
+	"汉":       "han",
+	"汉 ":      "han ",
+	" 汉":      " han",
+	"汉 字":     "han zi",
+	" 汉 字":    " han zi",
+	" 汉 字 ":   " han zi ",
 	"HAN汉ZI字": "HAN-han-ZI-zi",
 	"HAN汉字ZI": "HAN-han-zi-ZI",
 }
 
-func TestJoinWithSpace (t *testing.T) {
+func TestJoinWithSpace(t *testing.T) {
 	for k, v := range fixturesWithSpace {
 		r := Convert(k, " ")
 		if r != v {
@@ -50,7 +50,7 @@ func TestJoinWithSpace (t *testing.T) {
 	}
 }
 
-func TestJoinWithDash (t *testing.T) {
+func TestJoinWithDash(t *testing.T) {
 	for k, v := range fixturesWithDash {
 		r := Convert(k, "-")
 		if r != v {
