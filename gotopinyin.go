f0cc398b180cbef113a8c8658043cea796cee298
elvuel <elvuel@gmail.com>
2018-07-31 11:11:36 +0800

    revise

--- gotopinyin.go
+++ gotopinyin.go
@@ -1,59 +1,69 @@
 package gotopinyin
 
 import (
+	"bytes"
 	"regexp"
 	"strconv"
 	"strings"
+	"unicode/utf8"
+)
+
+var (
+	digitalReg = regexp.MustCompile("\\d")
 )
 
 func toHex(x int64) string {
 	return strconv.FormatInt(x, 16)
 }
 
-func replaceWith(r *regexp.Regexp, s, with string) string {
-	return string(r.ReplaceAll([]byte(s), []byte(with)))
+func replaceWith(s, with string) string {
+	return string(digitalReg.ReplaceAll([]byte(s), []byte(with)))
 }
 
 func Convert(s, join string) string {
-	pyStr := ""
-	runes := []rune(s)
-	digitalReg := regexp.MustCompile("\\d")
+	if !utf8.ValidString(s) {
+		return ""
+	}
 
+	var buf bytes.Buffer
+	runes := []rune(s)
+
+	var x int64
+	var chr, sHex, pyList string
 	for i, runesLen := 0, len(runes); i < runesLen; i++ {
-		x := int64(runes[i])
-		var chr string
+		x = int64(runes[i])
 		if x > 255 {
-			sHex := strings.ToUpper(toHex(x))
-			if pyList := DICT[sHex]; pyList != "" {
-				chr = replaceWith(digitalReg, pyList, "")
+			sHex = strings.ToUpper(toHex(x))
+			if pyList = DICT[sHex]; pyList != "" {
+				chr = replaceWith(pyList, "")
 			} else {
 				chr = string(x)
 			}
 			if i == 0 {
-				pyStr += chr
+				buf.WriteString(chr)
 				if runesLen > (i + 1) {
 					if runes[i+1] <= 255 {
 						if runes[i+1] != 32 {
-							pyStr += join
+							buf.WriteString(join)
 						}
 					}
 				}
 			} else {
 				if i != 0 && runes[i-1] != 32 {
-					pyStr += join
+					buf.WriteString(join)
 				}
-				pyStr += chr
+				buf.WriteString(chr)
 				if runesLen > (i + 1) {
 					if runes[i+1] <= 255 {
 						if runes[i+1] != 32 {
-							pyStr += join
+							buf.WriteString(join)
 						}
 					}
 				}
 			}
 		} else {
-			pyStr += string(x)
+			buf.WriteString(string(x))
 		}
 	}
-	return pyStr
+	return buf.String()
 }
